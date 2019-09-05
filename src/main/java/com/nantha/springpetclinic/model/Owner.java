dc55bbcfc7b815be2d6e92343a008f4a8f5e5739
Nantha Kumar G <nantha2011batch@gmail.com>
2019-09-05 16:38:58 +0530

    adding POJO. Closes #2

---
+++ src/main/java/com/nantha/springpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.nantha.springpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/nantha/springpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.nantha.springpetclinic.model;
+
+public class Vet extends Person {
+}
