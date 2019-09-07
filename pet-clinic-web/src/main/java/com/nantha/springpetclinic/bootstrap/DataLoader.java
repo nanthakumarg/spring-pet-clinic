077d78b00a83326cd31095b0ba2608e1d9bc207d
Nantha Kumar G <nantha2011batch@gmail.com>
2019-09-07 08:49:08 +0530

    Listed all vets in the index page. closes #15

--- pet-clinic-web/src/main/java/com/nantha/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/nantha/springpetclinic/bootstrap/DataLoader.java
@@ -43,7 +43,7 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(1L);
+        vet2.setId(2L);
         vet2.setFirstName("Sam");
         vet2.setLastName("Axe");
         vetService.save(vet2);
